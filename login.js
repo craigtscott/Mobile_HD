91f4218884e6531c3bc5b501d27f8721d7b66b55
Craig Scott <craigtscottjr@gmail.com>
2017-06-06 13:39:36 -0700

    haddle errors fixed

--- login.js
+++ login.js
@@ -1,3 +1,4 @@
+'use strict'
 import React, { Component } from 'react';
 import { Text,
         View,
@@ -19,66 +20,69 @@ class Login extends Component {
     super();
 
     this.state = {
-      user_name: "",
-      password: "",
+      user_name: "demo",
+      password: "password",
       error: "",
-    };
+      showProgress: false,
+    }
   }
 
-  // storeToken(responseData){
-  //   AsyncStorage.setItem(ACCESS_TOKEN, responseData, (err)=> {
-  //     if(err){
-  //       console.log("an error");
-  //       throw err;
-  //     }
-  //     console.log("success");
-  //   }).catch((err)=> {
-  //       console.log("error is: " + err);
-  //   });
-  // }
+  storeToken(responseData){
+    AsyncStorage.setItem(ACCESS_TOKEN, responseData, (err)=> {
+      if(err){
+        console.log("an error");
+        throw err;
+      }
+      console.log("success");
+    }).catch((err)=> {
+        console.log("error is: " + err);
+    });
+  }
 
-  // async _handleSubmit() {
-  //   try {
-  //     let responce = await fetch('http://localhost:3000/api/session', {
-  //       method: 'POST',
-  //       headers: {
-  //           'Accept': 'application/json',
-  //           'Content-Type': 'application/json'
-  //         },
-  //       body: JSON.stringify({
-  //         user:{
-  //           user_name: "A",
-  //           password: "password",
-  //         }
-  //       })
-  //     });
-  //   let res = await responce.text();
-  //   if (response.status >= 200 && response.status < 300) {
-  //         //Handle success
-  //         let accessToken = res;
-  //         console.log(accessToken);
-  //         //On success we will store the access_token in the AsyncStorage
-  //         // this.storeToken(accessToken);
-  //         // this.redirect('home');
-  //   } else {
-  //     //Handle error
-  //         let error = res;
-  //         throw error;
-  //     }
-  //   } catch(error) {
-  //      this.setState({error: error});
-  //      console.log("error " + error);
-  //      this.setState({showProgress: false});
-  //  }
-    // const user = Object.assign({}, this.state);
-    // this.props.processForm(user)
-    // .then(() =>
-    //   this.props.navigator.push({ name: "Lists" }));
-  // }
+  async onLoginPressed() {
+    try {
+      let response = await fetch('http://localhost:3000/api/session', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          },
+        body: JSON.stringify({
+          user:{
+            user_name: this.state.user_name,
+            password: this.state.password,
+            mobile: true,
+          }
+        })
+      });
+      debugger;
+    let res = await response.text();
+    if (response.status >= 200 && response.status < 300) {
+          //Handle success
+          let accessToken = res;
+          console.log(accessToken);
+          //On success we will store the access_token in the AsyncStorage
+          this.storeToken(accessToken);
+          // this.redirect('home');
+    } else {
+      //Handle error
+        let error = res;
+        throw error;
+      }
+    } catch(error) {
+       this.setState({error: error});
+       console.log("error " + error);
+       this.setState({showProgress: false});
+   }
+    const user = Object.assign({}, this.state);
+    this.props.processForm(user)
+    .then(() =>
+      this.props.navigator.push({ name: "Lists" }));
+  }
 
-  // _signup() {
-  //   this.props.navigator.push({ name: "SignUp"});
-  // }
+  _signup() {
+    this.props.navigator.push({ name: "SignUp"});
+  }
 
   render() {
     var TouchableElement = TouchableHighlight;
@@ -108,7 +112,7 @@ class Login extends Component {
       <View style={styles.height}>
         <TouchableElement
           style={styles.button}
-          onPress={() => this._handleSubmit.bind(this) }>
+          onPress={this.onLoginPressed.bind(this)}>
           <Text style={styles.text}> Login </Text>
         </TouchableElement>
       </View>
@@ -117,11 +121,11 @@ class Login extends Component {
         title="Sign Up"
         color="#841584"
       />
-      <Text>
-         {this.state.error}
-       </Text>
 
       </View>
+      <Text>
+      {this.state.error}
+      </Text>
     </View>
     );
 
